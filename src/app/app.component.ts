d23c2f9f637a020b72f409f708962d50e4f8f498
galicians <galicians@gmail.com>
2020-05-02 19:55:01 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+
+
+  nameControl = new FormControl('Pablo');
+  name = this.nameControl.value;
+
+
 }
