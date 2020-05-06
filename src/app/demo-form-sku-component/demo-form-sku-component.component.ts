38a6f418dc41c0cab2a893dcf2d1f2214df458a7
galicians <galicians@gmail.com>
2020-05-06 08:41:50 +0100

    2 files modified

--- src/app/demo-form-sku-component/demo-form-sku-component.component.ts
+++ src/app/demo-form-sku-component/demo-form-sku-component.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-demo-form-sku-component',
   templateUrl: './demo-form-sku-component.component.html',
-  styleUrls: ['./demo-form-sku-component.component.css']
-})
+  styleUrls: ['./demo-form-sku-component.component.css']})
+  
 export class DemoFormSkuComponentComponent implements OnInit {
 
   constructor() { }
